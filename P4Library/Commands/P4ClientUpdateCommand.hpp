3a39821f4997e33a7aeffb4a6aec852ce3a7ebc3
sipe9 <ssipen@gmail.com>
2015-04-06 16:39:24 +0300

    asdf

---
+++ P4Library/Commands/P4ClientUpdateCommand.hpp
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include "../P4Task.hpp"
+#include "P4Command.hpp"
+#include "P4Client.hpp"
+
+#include <unordered_map>
+
+namespace VersionControl
+{
+	class P4ClientUpdateCommand : public P4Command
+	{
+
+	public:		
+
+		P4ClientUpdateCommand(const P4ClientData &clientData);
+
+		virtual bool Run(P4Task &task, const CommandArgs &args);
+
+		void OutputInfo(char level, const char *data);		
+
+		virtual void InputData(StrBuf *strbuf, Error *e);
+
+	private:
+
+		static std::string CreateClientInputBuffer(const P4ClientData &clientData);
+
+	private:
+
+		bool			m_clientUpdated;
+		
+		P4ClientData	m_clientData;
+	};
+}
